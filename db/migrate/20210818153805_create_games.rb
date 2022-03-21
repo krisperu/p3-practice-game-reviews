498ac927431eadae6ae0bc24200c5d9f064efe35
betalantz <betalantz@gmail.com>
2022-03-21 16:42:14 -0700

    completes Category --< Game relationship

--- db/migrate/20210818153805_create_games.rb
+++ db/migrate/20210818153805_create_games.rb
@@ -3,6 +3,7 @@ class CreateGames < ActiveRecord::Migration[6.1]
     create_table :games do |t|
       t.string :name
       t.string :platform
+      t.references :category
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,20 +3,22 @@ User.destroy_all
 Game.destroy_all
 Category.destroy_all
 
+Faker::Game.unique.clear
+
 puts "Creating users..."
 10.times do
     User.create(username: Faker::Internet.username, email: Faker::Internet.email)
-end
-
-puts "Creating games..."
-10.times do 
-    Game.create(name: Faker::Game.title, platform: Faker::Game.platform)
 end
 
 puts "Creating categories..."
 categories = ["First-person shooter", "Puzzle", "Platformer", "RPG", "MMO", "Adventure"]
 categories.each do |category|
     Category.create(name: category)
+end
+
+puts "Creating games..."
+10.times do 
+    Game.create(name: Faker::Game.unique.title, platform: Faker::Game.platform, category: Category.all.sample)
 end
 
 # TODO: Create seeds for the models/migrations you have added
