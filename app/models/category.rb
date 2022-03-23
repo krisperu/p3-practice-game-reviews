886806d0e5176d2be8085d8aed15867bc36fe8d0
Kristina Peru <krissy.germanov@gmail.com>
2022-03-23 16:55:05 -0600

    changes some stuff

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,12 @@
 class Category < ActiveRecord::Base
     has_many :games
+
+    def self.highest_rated
+        self.all.max_by { |r| r.reviews.rating }
+    end
+
+    def platforms 
+        self.games.pluck(:platform).uniq
+    end
+
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -3,9 +3,12 @@ class Game < ActiveRecord::Base
     has_many :users, through: :reviews
     belongs_to :category
     
+    def self.find_by_platform(platform)
+        self.all.pluck(:game)
+    end
+    
     def average_rating
         # binding.pry
-
         rate = self.reviews.average(:rating)
         rate ? rate : "No ratings yet"
     end
